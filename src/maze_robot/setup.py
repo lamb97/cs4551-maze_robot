428d552d2cae622367dc612b5fa428562c6f2022
lamb97 <3297892150@qq.com>
2025-12-13 15:53:56 -0600

    world_robot launch

--- src/maze_robot/setup.py
+++ src/maze_robot/setup.py
@@ -10,6 +10,14 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+
+        ('share/' + package_name + '/launch', [
+            'launch/world_turtlebot3.launch.py',
+        ]),
+
+        ('share/' + package_name + '/worlds', [
+            'worlds/maze.world',
+        ]),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
